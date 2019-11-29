0ea6b48098e11c631915cdf9563df74df5c8b5ad
Vicki Keeley <vicki.keeley@gmail.com>
2019-11-29 15:03:10 +0000

    styling for mood options

--- src/components/shared/OptionList.vue
+++ src/components/shared/OptionList.vue
@@ -1,15 +1,21 @@
 <template>
-  <ul>
-    <li v-for="(option, i) in options" :key="i" :class="option.type">
-      <label :for="`input-${option.type}-${i}`">
-        <input
-          type="radio"
-          :id="`input-${option.type}-${i}`"
-          v-model="selectedOption"
-          :value="option"
-        />
-        {{ option.text }}
-      </label>
+  <ul class="option-list">
+    <li
+      v-for="(option, i) in options"
+      :key="i"
+      :class="[option.type === 'mood' ? option.text : '', option.type]"
+    >
+      <v-chip :color="setColor(option)">
+        <label :for="`input-${option.type}-${i}`">
+          <input
+            type="radio"
+            :id="`input-${option.type}-${i}`"
+            v-model="selectedOption"
+            :value="option"
+          />
+          {{ option.text }}
+        </label>
+      </v-chip>
     </li>
   </ul>
 </template>
@@ -25,6 +31,38 @@ export default {
   setup(props, { emit }) {
     const selectedOption = ref(null)
 
+    function setColor(option) {
+      switch (option.text) {
+        case 'angry':
+        case 'frustrated':
+        case 'overwhelmed':
+        case 'stressed':
+        case 'annoyed':
+          return 'error darken-2'
+          break
+        case 'insecure':
+        case 'anxious':
+        case 'afraid':
+        case 'sad':
+        case 'ashamed':
+        case 'depressed':
+          return 'primary'
+          break
+        case 'okay':
+        case 'numb':
+        case 'tired':
+          return '#dcdcdc'
+          break
+        case 'calm':
+        case 'optimistic':
+        case 'happy':
+          return 'warning'
+          break
+        default:
+          return 'secondary'
+      }
+    }
+
     watch(
       () => selectedOption.value,
       newVal => {
@@ -34,10 +72,27 @@ export default {
       }
     )
 
-    return { selectedOption }
+    return { selectedOption, setColor }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.option-list {
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -10px;
+  padding: 0;
+
+  li {
+    position: relative;
+    margin: 0 10px 10px 0;
+  }
+
+  input[type='radio'] {
+    position: absolute;
+    visibility: hidden;
+  }
+}
+</style>
